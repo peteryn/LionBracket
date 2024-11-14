13d0bb08e59630149fe456a750670b6668c62658
Peter Yuan <peter.yuan18@gmail.com>
2024-11-14 10:22:13 -0500

    Add function call to update internal swiss object

--- src/nodes/RoundNodeComponent.tsx
+++ src/nodes/RoundNodeComponent.tsx
@@ -1,45 +1,66 @@
 import { Handle, Position, type NodeProps, type Edge } from "@xyflow/react";
 import { type RoundNodeComponent } from "./types";
+import { swiss } from ".";
+import { getScore } from "../helper/score";
 
-export function RoundNodeComponent({data}: NodeProps<RoundNodeComponent>) {
-	const matches = data.roundNode.matches.map(match => {
+export function RoundNodeComponent({ data }: NodeProps<RoundNodeComponent>) {
+	const matches = data.roundNode.matches.map((match) => {
 		if (match.matchRecord) {
+			const upperInputId = `${match.id}upper`;
+			const lowerInputId = `${match.id}lower`;
+
+			function onChange() {
+				const upperTeamWins = getScore(upperInputId);
+				const lowerTeamWins = getScore(lowerInputId);
+				const matchRecord = swiss.getMatchRecordById(match.id);
+				if (matchRecord) {
+					matchRecord.upperTeamWins = upperTeamWins;
+					matchRecord.lowerTeamWins = lowerTeamWins;
+					swiss.setMatchRecordById(match.id, matchRecord);
+					console.log(swiss.roundNodes.get("0-1")?.matches[0].matchRecord);
+				}
+			}
+
 			return (
 				<div key={match.id} className="matches-area">
 					<table>
 						<tr>
 							<td>{match.matchRecord.upperTeam.seed}</td>
-							<td><input type="text" style={{ width: 10}} className="nodrag"/></td>
+							<td>
+								<input
+									id={upperInputId}
+									type="text"
+									style={{ width: 10 }}
+									className="nodrag"
+									onChange={onChange}
+								/>
+							</td>
 							<td>vs</td>
-							<td><input type="text" style={{ width: 10}} className="nodrag"/></td>
+							<td>
+								<input
+									id={lowerInputId}
+									type="text"
+									style={{ width: 10 }}
+									className="nodrag"
+									onChange={onChange}
+								/>
+							</td>
 							<td>{match.matchRecord.lowerTeam.seed}</td>
 						</tr>
 					</table>
 				</div>
 			);
 		} else {
-			return <div key={match.id}>
-				TBD vs TBD
-			</div>;
+			return <div key={match.id}>TBD vs TBD</div>;
 		}
 	});
 	return (
 		// We add this class to use the same styles as React Flow's default nodes.
 		<div className="react-flow__node-default">
-			<Handle
-				type="target"
-				position={Position.Left}
-				id={"a"}
-			/>
+			<Handle type="target" position={Position.Left} id={"a"} />
 			<p>{data.name}</p>
-			<div>
-				{matches}
-			</div>
-			<Handle
-				type="source"
-				position={Position.Right}
-				id={"b"}
-			/>
+			<div>{matches}</div>
+			<Handle type="source" position={Position.Right} id={"b"} />
 		</div>
 	);
 }
