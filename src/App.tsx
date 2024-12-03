64de1ed833491e7631e09a19ac9582f0e5de1eca
Peter Yuan <peter.yuan18@gmail.com>
2024-12-02 19:14:34 -0500

    Remove code from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,67 +21,25 @@ import { createSwissNodes, nodeTypes } from "./nodes";
 import { initialEdges, edgeTypes } from "./edges";
 import { SwissBracket } from "../BracketLion/SwissBracket";
 
-// UI can render future rounds correctly
-// UI problem has to deal with the useEffect
-// const round1 = globalSwiss.rootRound;
-// round1.matches.forEach((match) => {
-// 	const mr = globalSwiss.getMatchRecordById(match.id);
-// 	if (!mr) {
-// 		console.log("big problem");
-// 	}
-// 	mr!.upperTeamWins = 1;
-// 	globalSwiss.setMatchRecordById(match.id, mr!);
-// });
-// console.log(globalSwiss.roundNodes.get("1-0")?.matches[0]);
-
 export default function App() {
 	const [swissB, setSwissB] = useState(globalSwiss.data);
-
-	const [myString, setMyString] = useState("default");
 
 	const initialNodes = createSwissNodes(globalSwiss);
 
 	const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
 	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-	// nodes.forEach((node) => (node.data.parentSwissBracket = swissB));
 	nodes.map((node) => {
 		node.data.parentSwissBracket = swissB;
 	});
 	nodes.forEach((node) => (node.data.updateSwissFun = setSwissB));
-	nodes.forEach((node) => (node.data.setMyString = setMyString));
-
-	nodes.map((node) => {
-		if (node.id === "1-0") {
-			node.data.message = myString;
-		}
-	});
-
-	// useEffect(() => {
-	// 	console.log("in use effect");
-	// 	setNodes((nds) =>
-	// 		nds.map((node) => {
-	// 			if (node.id === "1-0") {
-	// 				// it's important that you create a new node object
-	// 				// in order to notify react flow about the change
-	// 				return {
-	// 					...node,
-	// 					style: {
-	// 						...node.style,
-	// 					},
-	// 				};
-	// 			}
-	// 			return node;
-	// 		})
-	// 	);
-	// }, [swissB, setNodes, myString]);
 
 	useEffect(() => {
 		console.log("in use effect");
-		const updatedNodes = createSwissNodes(globalSwiss)
+		const updatedNodes = createSwissNodes(globalSwiss);
 		setNodes(updatedNodes);
 		console.log();
-	}, [swissB, setNodes, myString]);
+	}, [swissB, setNodes]);
 
 	return (
 		<ReactFlow
