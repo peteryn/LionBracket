646a23f732db92b5b9ec131bb9a2e47a130fa4b6
Peter Yuan <peter.yuan18@gmail.com>
2024-12-09 12:11:32 -0500

    Refactor code out of round node component

--- src/nodes/RoundNodeComponent.tsx
+++ src/nodes/RoundNodeComponent.tsx
@@ -3,8 +3,23 @@ import { type RoundNodeComponent } from "./types.ts";
 import { globalSwiss } from "../App.tsx";
 import VersusRoundComponent from "./VersusRoundComponent.tsx";
 import { addColor } from "../helper/color.ts";
+import { RoundNodeType } from "./RoundNodeType.ts";
 
 export function RoundNodeComponent({ data }: NodeProps<RoundNodeComponent>) {
+	const matchesComponents = createMatches(data);
+	const classes = createRoundCSS(data.name);
+	return (
+		// We add this class to use the same styles as React Flow's default nodes.
+		<div className="react-flow__node-default">
+			<p className={classes}>{data.name}</p>
+			<Handle type="target" position={Position.Left} id={data.inputHandleId} />
+			<div className="versus-area">{matchesComponents}</div>
+			<Handle type="source" position={Position.Right} id={data.outputHandleId} />
+		</div>
+	);
+}
+
+export function createMatches(data: RoundNodeType) {
 	const matches = globalSwiss.data.roundNodes.get(data.name)?.matches;
 	if (!matches) {
 		throw new Error();
@@ -16,20 +31,16 @@ export function RoundNodeComponent({ data }: NodeProps<RoundNodeComponent>) {
 			updateSwissFun={data.updateSwissFun}
 		></VersusRoundComponent>
 	));
+	return matchesComponents;
+}
+
+export function createRoundCSS(name: string) {
 	let classes = "versus-section-round-title bourgeois ";
-	classes = addColor(data.name, classes, [
+	classes = addColor(name, classes, [
 		"round-winning",
 		"round-middle",
 		"round-losing",
 		"round-start",
 	]);
-	return (
-		// We add this class to use the same styles as React Flow's default nodes.
-		<div className="react-flow__node-default">
-			<p className={classes}>{data.name}</p>
-			<Handle type="target" position={Position.Left} id={data.inputHandleId} />
-			<div className="versus-area">{matchesComponents}</div>
-			<Handle type="source" position={Position.Right} id={data.outputHandleId} />
-		</div>
-	);
+	return classes;
 }
