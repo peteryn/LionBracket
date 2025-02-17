c52c6f0935caa6caf24c4efc6df4441cf53e7375
Peter Yuan <peter.yuan18@gmail.com>
2025-02-17 17:15:12 -0500

    Format code

--- src/nodes/RoundNodeType.ts
+++ src/nodes/RoundNodeType.ts
@@ -2,37 +2,33 @@ import { RoundNode } from "../../LionBracketEngine/src/models/round_node";
 import { SwissBracketFlow } from "../../LionBracketEngine/src/swiss_bracket/swiss_backet_flow";
 
 export class RoundNodeType {
-  [key: string]: unknown;
-  name: string;
-  roundNode: RoundNode;
-  swissBracket: SwissBracketFlow;
-  updateSwissFun:
-    | React.Dispatch<React.SetStateAction<RoundNode>>
-    | undefined;
-  inputHandleId: string;
-  outputHandleId: string;
-  qualifiedHandleId: string;
-  eliminatedHandleId: string;
+	[key: string]: unknown;
+	name: string;
+	roundNode: RoundNode;
+	swissBracket: SwissBracketFlow;
+	updateSwissFun: React.Dispatch<React.SetStateAction<RoundNode>> | undefined;
+	inputHandleId: string;
+	outputHandleId: string;
+	qualifiedHandleId: string;
+	eliminatedHandleId: string;
 
-  constructor(
-    name: string,
-    roundNode: RoundNode,
-    swissBracket: SwissBracketFlow,
-    updateSwissFun:
-      | React.Dispatch<React.SetStateAction<RoundNode>>
-      | undefined,
-    inputHandleId: string,
-    outputHandleId: string,
-    qualifiedHandleId: string,
-    eliminatedHandleId: string
-  ) {
-    this.name = name;
-    this.roundNode = roundNode;
-    this.swissBracket = swissBracket;
-    this.updateSwissFun = updateSwissFun;
-    this.inputHandleId = inputHandleId;
-    this.outputHandleId = outputHandleId;
-    this.qualifiedHandleId = qualifiedHandleId;
-    this.eliminatedHandleId = eliminatedHandleId;
-  }
+	constructor(
+		name: string,
+		roundNode: RoundNode,
+		swissBracket: SwissBracketFlow,
+		updateSwissFun: React.Dispatch<React.SetStateAction<RoundNode>> | undefined,
+		inputHandleId: string,
+		outputHandleId: string,
+		qualifiedHandleId: string,
+		eliminatedHandleId: string
+	) {
+		this.name = name;
+		this.roundNode = roundNode;
+		this.swissBracket = swissBracket;
+		this.updateSwissFun = updateSwissFun;
+		this.inputHandleId = inputHandleId;
+		this.outputHandleId = outputHandleId;
+		this.qualifiedHandleId = qualifiedHandleId;
+		this.eliminatedHandleId = eliminatedHandleId;
+	}
 }
