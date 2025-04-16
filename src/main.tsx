f9fe33f7e66932e99d271a467a0103c9c43e5251
Peter Yuan <peter.yuan18@gmail.com>
2025-04-16 13:04:22 -0400

    Add basic navigation and routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
+import App from "./App";
 
-import './index.css';
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Birmingham from "./Birmingham";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}></Route>
+				<Route path="/birmingham" element={<Birmingham />}></Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
 );
