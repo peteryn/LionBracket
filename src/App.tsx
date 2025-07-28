7a49a0c68000b8f1ddbbe53e4fd3f93bca02d024
peteryn <peter.yuan18@gmail.com>
2025-07-28 15:12:50 -0400

    Refactor region cards

--- src/App.tsx
+++ src/App.tsx
@@ -26,78 +26,69 @@ export default function App() {
 					regionName="Europe"
 					color="round-start-text"
 					data={[
-						"/europe_open_4",
-						"/europe_open_5",
-						"/europe_open_6",
+						["/europe_open_4", "Open #4"],
+						["/europe_open_5", "Open #5"],
+						["/europe_open_6", "Open #6"],
 					]}
-					data1v1={undefined}
-					datalcq={undefined}
 				/>
 				<RegionCard
 					regionName="North America"
 					color="round-losing-text"
 					data={[
-						"/north_america_open_4",
-						"/north_america_open_5",
-						"/north_america_open_6",
+						["/north_america_open_4", "Open #4"],
+						["/north_america_open_5", "Open #5"],
+						["/north_america_open_6", "Open #6"],
+						["/north_america_open_1v1", "1v1"],
+						["/north_america_lcq", "LCQ"],
 					]}
-					data1v1={"/north_america_open_1v1"}
-					datalcq={"/north_america_lcq"}
 				/>
 				<RegionCard
 					regionName="South America"
 					color="round-middle-text"
 					data={[
-						"/south_america_open_4",
-						"/south_america_open_5",
-						"/south_america_open_6",
+						["/south_america_open_4", "Open #4"],
+						["/south_america_open_5", "Open #5"],
+						["/south_america_open_6", "Open #6"],
 					]}
-					data1v1={undefined}
-					datalcq={undefined}
 				/>
 				<RegionCard
 					regionName="Middle East & North Africa"
 					color="round-winning-text"
 					data={[
-						"/middle_east_north_africa_open_4",
-						"/middle_east_north_africa_open_5",
-						"/middle_east_north_africa_open_6",
+						["/middle_east_north_africa_open_4", "Open #4"],
+						["/middle_east_north_africa_open_5", "Open #5"],
+						["/middle_east_north_africa_open_6", "Open #6"],
+						["/middle_east_north_africa_open_1v1", "1v1"],
 					]}
-					data1v1={"/middle_east_north_africa_open_1v1"}
-					datalcq={undefined}
 				/>
 				<RegionCard
 					regionName="Oceania"
 					color="purple-text"
 					data={[
-						"/oceania_open_4",
-						"/oceania_open_5",
-						"/oceania_open_6",
+						["/oceania_open_4", "Open #4"],
+						["/oceania_open_5", "Open #5"],
+						["/oceania_open_6", "Open #6"],
+						["/oceania_1v1", "1v1"],
 					]}
-					data1v1={"/oceania_1v1"}
-					datalcq={undefined}
 				/>
 				<RegionCard
 					regionName="Asia-Pacific"
 					color="pink-text"
 					data={[
-						"/asia_pacific_open_4",
-						"/asia_pacific_open_5",
-						"/asia_pacific_open_6",
+						["/asia_pacific_open_4", "Open #4"],
+						["/asia_pacific_open_5", "Open #5"],
+						["/asia_pacific_open_6", "Open #6"],
 					]}
-					data1v1={undefined}
-					datalcq={undefined}
 				/>
 				<RegionCard
 					regionName="Sub-Saharan Africa"
 					color="orange-text"
 					data={[
-						"/sub_saharan_africa_open_4",
-						"/sub_saharan_africa_open_5",
-						"/sub_saharan_africa_open_6",
+						["/sub_saharan_africa_open_4", "Open #4"],
+						["/sub_saharan_africa_open_5", "Open #5"],
+						["/sub_saharan_africa_open_6", "Open #6"],
+						["/sub_saharan_africa_1v1", "1v1"],
 					]}
-					data1v1={"/sub_saharan_africa_1v1"}
-					datalcq={undefined}
 				/>
 			</div>
 		</div>
--- src/components/RegionCard.tsx
+++ src/components/RegionCard.tsx
@@ -4,34 +4,16 @@ export default function ({
 	regionName,
 	data,
 	color,
-	data1v1,
-	datalcq,
 }: {
 	regionName: string;
-	data: string[];
+	data: string[][];
 	color: string;
-	data1v1: string | undefined;
-	datalcq: string | undefined;
 }) {
 	const items = data.map((item, index) => (
 		<li key={index}>
-			<NavLink to={item}>Open #{index + 4}</NavLink>
+			<NavLink to={item[0]}>{item[1]}</NavLink>
 		</li>
 	));
-	if (data1v1) {
-		items.push(
-			<li key={data.length}>
-				<NavLink to={data1v1}>1v1</NavLink>
-			</li>
-		);
-	}
-	if (datalcq) {
-		items.push(
-			<li key={data.length}>
-				<NavLink to={datalcq}>LCQ</NavLink>
-			</li>
-		);
-	}
 	return (
 		<div className="bebas-neue-regular">
 			<h2 className={color}>{regionName}</h2>
